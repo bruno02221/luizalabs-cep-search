ec01caf0169910ce08f0187c33e11e7402bf8b83
Bruno <brunodev02221@gmail.com>
2018-04-16 16:13:43 -0300

    Implement embedded map component

--- src/commons/EmbeddedMap.js
+++ src/commons/EmbeddedMap.js
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
-const EmbeddedMap = () => <div>Not implemented yet</div>;
+const EmbeddedMap = ({ street, city, state, district }) => {
+  const apiKey = "AIzaSyA-M0jiYyWeks2H4RXskG0jvs3ie4X2Jjw";
+  const query = `${district}, ${street}, ${city} - ${state}`;
+  const zoom = 15;
+  const src = `https://www.google.com/maps/embed/v1/place?q=${query}&zoom=${zoom}&key=${apiKey}`;
+  return <MapFrame src={src} frameBorder="0" />;
+};
+
+const MapFrame = styled.iframe`
+  width: 100%;
+  height: 400px;
+`;
 
 export default EmbeddedMap;
